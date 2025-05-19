f005509d6ca1be9af131b1d27d723a8ec086517c
acapsambelis <acapsamb@gmail.com>
2025-05-19 13:34:21 -0400

    Added snake game demo

---
+++ Caps.RPG.GameApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Caps.RPG.GameApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using var game = new TutorialGame();
+            game.Run();
+        }
+    }
+}
