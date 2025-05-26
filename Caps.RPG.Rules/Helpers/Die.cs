f3975d8f54feee56983a6a259dbbe49639c4e1b6
acapsambelis <acapsamb@gmail.com>
2025-05-25 22:29:23 -0400

    Cleaned Die class/enum

--- Caps.RPG.Rules/Helpers/Die.cs
+++ Caps.RPG.Rules/Helpers/Die.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace Caps.RPG.Rules.Helpers
+﻿namespace Caps.RPG.Rules.Helpers
 {
     public enum Die
     {
@@ -21,32 +19,7 @@ namespace Caps.RPG.Rules.Helpers
 
         public static int Roll(this Die die)
         {
-            return new DieInterior(die.Size()).Roll();
-        }
-    }
-
-    internal class DieInterior
-    {
-        private readonly int size;
-        public DieInterior(int size)
-        {
-            this.size = size;
-        }
-
-        public override bool Equals(object? obj)
-        {
-            return obj is DieInterior die &&
-                   size == die.size;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(size);
-        }
-
-        public virtual int Roll()
-        {
-            return Helpers.Roll.RollDie(size);
+            return Helpers.Roll.RollDie(die.Size());
         }
     }
 }
